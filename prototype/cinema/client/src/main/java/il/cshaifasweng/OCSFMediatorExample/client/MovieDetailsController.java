ae7f116b9536c16b333628ad4345adfbe5d3ec02
MayasGhantous <102159871+MayasGhantous@users.noreply.github.com>
2024-08-07 20:37:11 +0300

    create_files

---
+++ prototype/cinema/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/MovieDetailsController.java
@@ -0,0 +1,7 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import il.cshaifasweng.OCSFMediatorExample.entities.Movie;
+
+public class MovieDetailsController {
+    public static Movie current_movie;
+}
