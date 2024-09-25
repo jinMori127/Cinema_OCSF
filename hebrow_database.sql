205c6a26fb9be868ab51acb702f17b01e12a40ae
sami <sami19.serhan@gmail.com>
2024-09-25 19:44:50 +0300

    wp - data base add movie without link

--- hebrow_database.sql
+++ hebrow_database.sql
@@ -8,7 +8,16 @@ VALUES
 ('Anyone But You - כל אחד מלבדך', 'Sydney Sweeney, Glen Powell', 'Romance', 'romantic comedy about two ex-lovers who must pretend to be a couple again for a wedding', '1:44', 2023,'Mias',40,9,'https://w27.my-cima.net/watch.php?vid=7c7d539b0',0),
 ('Cruella - קרואלה', 'Emma Stone, Emma Thompson', 'Comedy', 'A stylish and rebellious origin story of Cruella de Vil, set in 1970s London amidst the punk rock revolution', '2:14', 2021,'Sami',50,6,'https://w27.my-cima.net/watch.php?vid=7c7d539b0',0),
 ('Divergent - מפוצלים', 'Shailene Woodley, Theo James', 'Action', 'In a dystopian future, a young woman discovers she is a Divergent and must uncover the secrets behind her societys facade', '2:19', 2014,'Aisha',30,7,'https://w27.my-cima.net/watch.php?vid=7c7d539b0',0),
-('Spider-Man: No Way Home - ספיידרמן: אין דרך הביתה', 'Tom Holland, Zendaya, Benedict Cumberbatch', 'Action', 'Spider-Man seeks Doctor Stranges help to restore his secret identity, unleashing multiverse chaos', '2:30', 2021,'Jan',30,1, 'https://w27.my-cima.net/watch.php?vid=7c7d539b0',0);
+('Spider-Man: No Way Home - ספיידרמן: אין דרך הביתה', 'Tom Holland, Zendaya, Benedict Cumberbatch', 'Action', 'Spider-Man seeks Doctor Stranges help to restore his secret identity, unleashing multiverse chaos', '2:30', 2021,'Jan',30,1, 'https://w27.my-cima.net/watch.php?vid=7c7d539b0',0),
+('The Incredible Hulk - האלק המדהים', 'Edward Norton', 'Action',
+ 'The story of Dr. Bruce Banner, who seeks a cure to his unique condition, which causes him to turn into a giant green monster under emotional stress, is depicted.',
+'1:52',2008,'Christian Volckman',25 ,7,'',0),
+('The Room - החדר', 'Edward Norton', 'Fantasy',
+ 'Matt and Kate buy an isolated house. While moving, they discover a strange room that grants them an unlimited number of material wishes. But since Kate has had two miscarriages, what they long for the most is a child.',
+'1:40' , 2019 ,'Christian Volckman',30, 6,'',0),
+('Incoming - הבאים', 'Raphael Alejandro', 'Comedy',
+ 'Four freshmen navigate the terrors of adolescence at their first-ever high school party.',
+'1:31' , 2024 ,'Dave Chernin',30, 6,'',0);
 
 
 truncate table worker;
